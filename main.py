2b22642db4268b1f4a992f3eb7d0f742fda08ff9
Adhrit Gupta <powercreatorpoint@gmail.com>
2026-08-22 19:03:36 +0400

    initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import torch
+from PIL import Image
+from transformers import (
+    AutoProcessor,
+    AutoModelForZeroShotObjectDetection
+)
+
+MODEL_ID = "IDEA-Research/grounding-dino-tiny"
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+print("Device:", device)
+print("Loading model...")
+processor = AutoProcessor.from_pretrained(MODEL_ID)
+model = AutoModelForZeroShotObjectDetection.from_pretrained(
+    MODEL_ID
+).to(device)
+print("Model loaded!")
+image = Image.open("fridge.jpg").convert("RGB")
+text = """
+tomato.
+potato.
+apple.
+banana.
+orange.
+carrot.
+cucumber.
+onion.
+"""
+
+inputs = processor(
+    images=image,
+    text=text,
+    return_tensors="pt"
+).to(device)
+
+with torch.no_grad():
+    outputs = model(**inputs)
+
+results = processor.post_process_grounded_object_detection(
+    outputs,
+    inputs.input_ids,
+    threshold=0.30,
+    text_threshold=0.25,
+    target_sizes=[image.size[::-1]]
+)
+
+result = results[0]
+
+for box, score, label in zip(
+    result["boxes"],
+    result["scores"],
+    result["text_labels"]
+):
+    print(
+        f"{label}: {score.item():.3f} "
+        f"box={box.tolist()}"
+    )
